1e8365eab6956b3a2f353ab0ba99e2614880ff8b
daffakurnia11 <72376750+daffakurnia11@users.noreply.github.com>
2022-08-27 18:07:36 +0700

    Creating layouts

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,45 @@
+<!--start sidebar -->
+<aside class="sidebar-wrapper" data-simplebar="true">
+  <div class="sidebar-header">
+    <div>
+      <img src="assets/images/logo-icon.png" class="logo-icon" alt="logo icon">
+    </div>
+    <div>
+      <h4 class="logo-text">Skodash</h4>
+    </div>
+    <div class="toggle-icon ms-auto">
+      <i class="bi bi-chevron-double-left"></i>
+    </div>
+  </div>
+  <!--navigation-->
+  <ul class="metismenu" id="menu">
+    <li>
+      <a href="javascript:;" class="has-arrow">
+        <div class="parent-icon"><i class="bi bi-house-door"></i>
+        </div>
+        <div class="menu-title">Dashboard</div>
+      </a>
+      <ul>
+        <li> <a href="index.html"><i class="bi bi-arrow-right-short"></i>eCommerce</a>
+        </li>
+        <li> <a href="index2.html"><i class="bi bi-arrow-right-short"></i>Sales</a>
+        </li>
+        <li> <a href="index3.html"><i class="bi bi-arrow-right-short"></i>Analytics</a>
+        </li>
+        <li> <a href="index4.html"><i class="bi bi-arrow-right-short"></i>Project Management</a>
+        </li>
+        <li> <a href="index5.html"><i class="bi bi-arrow-right-short"></i>CMS Dashboard</a>
+        </li>
+      </ul>
+    </li>
+    <li class="menu-label">UI Elements</li>
+      <a href="pages-user-profile.html">
+        <div class="parent-icon"><i class="bi bi-person-check"></i>
+        </div>
+        <div class="menu-title">User Profile</div>
+      </a>
+    </li>
+  </ul>
+  <!--end navigation-->
+</aside>
+<!--end sidebar -->
